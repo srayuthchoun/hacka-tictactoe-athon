4f212b6a9f271300f01280a8cd833cfef50c7f00
srayuth choun <srayuthchoun@gmail.com>
2016-02-10 15:44:17 -0800

    added cell array - Srayuth Choun

--- OnClick.js
+++ OnClick.js
@@ -1,10 +1,11 @@
 /**
- * Created by Srayuth Choun on 2/10/2016.
+ *  Created by Srayuth Choun on 2/10/2016.
  */
 
 var player_x = 1;
 var player_o = 0;
 var current_player = player_x;
+var cells = [null, null, null, null, null, null, null, null, null];
 
 $(document).ready(function(){
     $('.cell').click(function() {
@@ -14,12 +15,20 @@ $(document).ready(function(){
 });
 
 function player_turn (active_cell){
-    if (current_player == player_x){
-        $(active_cell).append("<img src='images/cutterkirby.png'>");
-        current_player = player_o;
+    if(cells[$(active_cell).index()] == null) {
+
+        if (current_player == player_x) {
+            $(active_cell).append("<img src='images/cutterkirby.png'>");
+            cells[$(active_cell).index()] = player_x;
+            current_player = player_o;
+        }
+        else {
+            $(active_cell).append("<img src='images/firekirby.png'>");
+            cells[$(active_cell).index()] = player_o;
+            current_player = player_x;
+        }
     }
-    else {
-        $(active_cell).append("<img src='images/firekirby.png'>");
-        current_player = player_x;
-    }
+    console.log($(active_cell).index());
+    console.log(cells);
+
 }
