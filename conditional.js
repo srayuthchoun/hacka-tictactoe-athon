48dc2f3543786a0455ed1eb812033f2e2aefd5a8
Timothy Nguyen <shimoshixp@gmail.com>
2016-02-10 11:55:25 -0800

    starting dynamic win condition

--- conditional.js
+++ conditional.js
@@ -5,27 +5,32 @@
 var cells = [];
 
 //hard coded conditionals for winning
-if (cells[0] == cells[1] && cells[1] == cells[2]) {
-    console.log("Someone won!");
+function winCondition() {
+    if (cells[0] == cells[1] && cells[1] == cells[2]) {
+        console.log("Someone won!");
+    }
+    else if (cells[0] == cells[3] && cells[3] == cells[6]) {
+        console.log("Someone won!");
+    }
+    else if (cells[0] == cells[4] && cells[4] == cells[8]) {
+        console.log("Someone won!");
+    }
+    else if (cells[1] == cells[4] && cells[4] == cells[7]) {
+        console.log("Someone won!");
+    }
+    else if (cells[3] == cells[4] && cells[4] == cells[5]) {
+        console.log("Someone won!");
+    }
+    else if (cells[2] == cells[5] && cells[5] == cells[8]) {
+        console.log("Someone won!");
+    }
+    else if (cells[6] == cells[7] && cells[7] == cells[8]) {
+        console.log("Someone won!");
+    }
+    else if (cells[2] == cells[4] && cells[4] == cells[6]) {
+        console.log("Someone won!");
+    }
 }
-else if (cells[0] == cells[3] && cells[3] == cells[6]) {
-    console.log("Someone won!");
-}
-else if (cells[0] == cells[4] && cells[4] == cells[8]) {
-    console.log("Someone won!");
-}
-else if (cells[1] == cells[4] && cells[4] == cells[7]) {
-    console.log("Someone won!");
-}
-else if (cells[3] == cells[4] && cells[4] == cells[5]) {
-    console.log("Someone won!");
-}
-else if (cells[2] == cells[5] && cells[5] == cells[8]) {
-    console.log("Someone won!");
-}
-else if (cells[6] == cells[7] && cells[7] == cells[8]) {
-    console.log("Someone won!");
-}
-else if (cells[2] == cells[4] && cells[4] == cells[6]) {
-    console.log("Someone won!");
+function winConditionDynamic() {
+
 }
