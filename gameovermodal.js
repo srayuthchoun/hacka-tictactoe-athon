7ea5f1711490ea5f311ac35aeb6da4038d1db80c
Deanna Sulli <dtcsulli@gmail.com>
2016-02-11 09:28:41 -0800

    made cells array change based on board size

---
+++ gameovermodal.js
@@ -0,0 +1,13 @@
+function gameovermodal(winner) {
+    switch(winner) {
+        case 'player1':
+            //show modal that player 1 won
+            break;
+        case 'player2':
+            //show modal that player 2 won
+            break;
+        default:
+            //show "draw"
+            break;
+    }
+}
