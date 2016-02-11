0771752f08725009b9b4f96ce2825b83c1613110
Timothy Nguyen <shimoshixp@gmail.com>
2016-02-10 23:48:19 -0800

    commented out winConditionV2, added a draw checker, need to implement win/draw modal functions

--- conditional.js
+++ conditional.js
@@ -1,8 +1,6 @@
 /**
  * Created by Timmy on 2/10/2016.
  */
-//declare array that holds cells
-var cells = [];
 //define boolean that acts as a flag for when game can be played or not
 var gameOver = false;
 
@@ -45,65 +43,115 @@ function winCondition() {
 //dynamic conditional function for winning
 function winConditionV2() {
     //define variable that is equal to the length of cells in a side
+    //this is done by finding the square root of the length of array
     var lengthOfSide = Math.sqrt(cells.length);
-    console.log("lengthOfSide:", lengthOfSide);
-    //for loop that checks every column for match
-    for (var x = 0; x < lengthOfSide; x++) {
-        for (var y = x; y < cells.length; y += lengthOfSide) {
+
+    //column checker
+    //for loop that acts as start point for every column
+    for (var column = 0; column < lengthOfSide; column++) {
+        //for loop that goes through current column
+        for (var y = column; y < cells.length; y += lengthOfSide) {
+            //if you reach the end of the column, then all elements are matching
+            //if current index of array is not null and the next incremented index is undefined
             if (cells[y] != null && cells[y + lengthOfSide] === undefined) {
                 console.log("Column Win!");
+                //set bool gameOver to true
                 gameOver = true;
+                //call function win modal here
+
+                //exit function
                 return;
             }
+            //else if current index of array is not the same as next increment of array
             else if (cells[y] != cells[y + lengthOfSide]) {
+                //exit immediate loop by breaking the loop condition
                 y = cells.length;
             }
-            console.log("looped, x=", x, "y=", y);
         }
-    }
-    //for loop that checks every row for match
-    for (var y = 0; y < cells.length; y += lengthOfSide) {
         //left to right diagonal check
-        if (y == 0) {
+        //if var column is at position of top left cell
+        if (column == 0) {
+            //for loop that goes from top left cell diagonally to bottom right cell
             for (var leftToRight = 0; leftToRight < cells.length; leftToRight += (lengthOfSide + 1)) {
+                //if you reach the end of the diagonal, then all elements are matching
+                //if current index of array is not null and the next incremented index is undefined
                 if (cells[leftToRight] != null && cells[leftToRight + lengthOfSide] === undefined) {
                     console.log("Diagonal Win!");
+                    //set bool gameOver to true
                     gameOver = true;
+                    //call function win modal here
+
+                    //exit function
                     return;
                 }
+                //else if current index of array is not the same as next increment of array
                 else if (cells[leftToRight] != cells[leftToRight + (lengthOfSide + 1)]) {
+                    //exit immediate loop by breaking the loop condition
                     leftToRight = cells.length;
                 }
-                console.log("looped, y=", y, "l2r=", leftToRight);
             }
         }
         //right to left diagonal check
-        else if (y == (cells.length - lengthOfSide)) {
+        //if var column is at position of top right cell
+        else if (column == (lengthOfSide - 1)) {
+            //for loop that goes from top right cell diagonally to bottom left cell
             for (var rightToLeft = (lengthOfSide - 1); rightToLeft < cells.length; rightToLeft += (lengthOfSide - 1)) {
+                //if you reach the end of the diagonal, then all elements are matching
+                //if current index of array is not null and the next incremented index is undefined
                 if (cells[rightToLeft] != null && cells[rightToLeft + lengthOfSide] === undefined) {
                     console.log("Diagonal Win!");
+                    //set bool gameOver to true
                     gameOver = true;
+                    //call function win modal here
+
+                    //exit function
                     return;
                 }
+                //else if current index of array is not the same as next increment of array
                 else if (cells[rightToLeft] != cells[rightToLeft + (lengthOfSide - 1)]) {
+                    //exit immediate loop by breaking the loop condition
                     rightToLeft = cells.length;
                 }
-                console.log("looped, y=", y, "r2l=", rightToLeft);
             }
         }
+    }
+    //row checker
+    //for loop that acts as start point for every row
+    for (var y = 0; y < cells.length; y += lengthOfSide) {
+        //define var that will keep track of how many times the loop runs
+        //this solved the problem with the next index of the end of row being the next row
         var numOfLoops = 0;
+        //for loop that goes through current row
         for (var x = y; x < (y + lengthOfSide); ++x) {
             //if (cells[x] != null && cells[x + 1] === undefined) {
             if (cells[x] != null && numOfLoops == (lengthOfSide - 1)) {
                 console.log("Row Win!");
+                //set bool gameOver to true
                 gameOver = true;
+                //call function win modal here
+
+                //exit function
                 return;
             }
+            //else if current index of array is not the same as next increment of array
             else if (cells[x] != cells[x + 1]) {
+                //exit immediate loop by breaking the loop condition
                 x = cells.length;
             }
+            //increment the count for times looped
             ++numOfLoops;
-            console.log("looped, y=", y, "x=", x);
         }
     }
+    //draw checker
+    //for loop that checks every element of array cells
+    for (var i = 0; i < cells.length; i++) {
+        //if current index of array is null,
+        if (cells[i] == null) {
+            //call function draw modal here
+
+            //exit function
+            return;
+        }
+    }
+    console.log("It's a draw!");
 }
