1d6f4394fd25edaf77504cf824b91618a0eefc06
Timothy Nguyen <shimoshixp@gmail.com>
2016-02-10 16:35:29 -0800

    v2 condition complete

--- conditional.js
+++ conditional.js
@@ -37,21 +37,53 @@ function winConditionDynamic() {
     //define variable that is equal to the length of cells in a side
     var lengthOfSide = Math.sqrt(cells.length);
     //for loop that checks every column for match
-    for (var verticalCheck = 0; verticalCheck < lengthOfSide; verticalCheck++) {
-
-    }
-    //for loop that checks every row for match
-    for (var horizontalCheck = 0; horizontalCheck < cells.length; horizontalCheck += (lengthOfSide - 1)) {
-        //left to right diagonal check
-        if (horizontalCheck == 0) {
-            for (var leftToRight = 0; leftToRight < cells.length; i++) {
-
+    for (var x = 0; x < lengthOfSide; x++) {
+        for (var y = x; y < cells.length; y += lengthOfSide) {
+            if (cells[y + lengthOfSide] == undefined) {
+                console.log("Winner!");
+                return;
+            }
+            else if (cells[y] != cells[y + lengthOfSide]) {
+                y = cells.length;
             }
         }
-        //right to left diagonal check
-        else if (horizontalCheck == (cells.length - lengthOfSide)) {
+
+        //for loop that checks every row for match
+        for (var y = 0; y < cells.length; y += lengthOfSide) {
+            //left to right diagonal check
+            if (y == 0) {
+                for (var leftToRight = 0; leftToRight < cells.length; leftToRight += (lengthOfSide + 1)) {
+                    if (cells[leftToRight + lengthOfSide] == undefined) {
+                        console.log("Winner!");
+                        return;
+                    }
+                    else if (cells[leftToRight] != cells[leftToRight + (lengthOfSide + 1)]) {
+                        leftToRight = cells.length;
+                    }
+                }
+            }
+            //right to left diagonal check
+            else if (y == (cells.length - lengthOfSide)) {
+                for (var rightToLeft = (lengthOfSide - 1); rightToLeft < cells.length; rightToLeft += (lengthOfSide - 1)) {
+                    if (cells[rightToLeft + lengthOfSide] == undefined) {
+                        console.log("Winner!");
+                        return;
+                    }
+                    else if (cells[rightToLeft] != cells[rightToLeft + (lengthOfSide - 1)]) {
+                        rightToLeft = cells.length;
+                    }
+                }
+            }
+            for (var x = y; x < (y + lengthOfSide); ++x) {
+                if (cells[x + 1] == undefined) {
+                    console.log("Winner!");
+                    return;
+                }
+                else if (cells[x] != cells[x + 1]) {
+                    x = cells.length;
+                }
+            }
 
         }
-
     }
 }
