44e2119c0295f99e27784b3447731405f8ef40c5
Deanna Sulli <dtcsulli@gmail.com>
2016-02-11 17:35:50 -0800

    added player stats

--- gameovermodal.js
+++ gameovermodal.js
@@ -3,10 +3,14 @@ function gameOverModal(winner) {
         case 'player1':
             //show modal that player 1 won
             $('#player1-win').show();
+            player1_wins++;
+            $('.player1-score .value').text(player1_wins);
             break;
         case 'player2':
             //show modal that player 2 won
             $('#player2-win').show();
+            player2_wins++;
+            $('.player2-score .value').text(player1_wins);
             break;
         default:
             //show "draw"
